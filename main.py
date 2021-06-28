63c226fb05e20659869cfc11a10b41a5e04399d3
Shinartic <mitchboz02@gmail.com>
2021-06-28 21:41:20 +0200

    Klaaaaar

--- main.py
+++ main.py
@@ -182,7 +182,21 @@ class Player():
                         dy = tile[1].top - self.rect.bottom
                         self.vel_y = 0
                         self.in_air = False
-
+            for platform in platform_group:
+                # check for collision in x direction
+                if platform.rect.colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    dx = 0
+                    # check for collision in y direction
+                if platform.rect.colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    # check if below the ground i.e. jumping
+                    if self.vel_y < 0:
+                        dy = platform.rect.bottom - self.rect.top
+                        self.vel_y = 0
+                    # check if above the ground i.e. falling
+                    elif self.vel_y >= 0:
+                        dy = platform.rect.top - self.rect.bottom
+                        self.vel_y = 0
+                        self.in_air = False
 
             if pygame.sprite.spritecollide(self, blob_group, False):
                 game_over = -1
